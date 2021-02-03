3764991515fa8046472c2c63e42b43e595f479e3
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-02-02 19:39:29 -0800

    got data to render

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import React from 'react'
 import './App.css';
 import {Route, Link, Switch} from "react-router-dom"
 import Display from "./Display"
+// import axios from 'axios'
 
 function App() {
   const url = "https://boxinggloves-mern.herokuapp.com"
+  const [gloves, setGloves] = React.useState([])
+ 	//function that does a fetch call to fetch items
+   const getGloves = () => {
+		fetch(url + '/gloves/')
+			.then((response) => response.json())
+			.then((data) => {
+				setGloves(data);
+			});
+	};
+
+	//get items on page load
+	React.useEffect(() => {
+		getGloves();
+	}, []);
+
+
+  
+
   return (
     <div className="App">
 <Switch>
-<Route exact path ="/">
-<Display />
-</Route>
+<Route exact path="/" render={(rp) => <Display 
+          {...rp} gloves={gloves.data}  />} />
+
 
 </Switch>
     </div>
--- src/Display.js
+++ src/Display.js
@@ -1,15 +1,24 @@
-import React from 'react'
-
-
+import React from "react";
 
 const Display = (props) => {
-return (
+  const { gloves } = props;
 
-    <h1>BOXING MERCH</h1>
-)
+  const loaded = () => (
+    <div style={{ textAlign: "center" }}>
+      {gloves.map((glove) => (
+        <article key={glove._id}>
+          <img src={glove.img} alt=''/>
+          <h1>{glove.brand}</h1>
+          <h3>{glove.size}</h3>
+        </article>
+      ))}
+    </div>
+  );
 
+  const loading = <h1>Loading...</h1>;
 
+  
+  return gloves ? loaded() : loading;
+};
 
-}
-
-export default Display
+export default Display;
