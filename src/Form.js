eb06154f061241d9be48b8761431c2f464e055ee
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-02-03 07:18:27 -0800

    got my placeholders in my form

--- src/Form.js
+++ src/Form.js
@@ -17,19 +17,22 @@ const Form = (props) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
+      <input 
+       placeholder= "Brand"
         type="text"
         name="brand"
         value={formData.brand}
         onChange={handleChange}
       />
       <input
+        placeholder="Size"
         type="number"
         name="size"
         value={formData.size}
         onChange={handleChange}
       />
       <input
+      placeholder="Img Url"
         type="text"
         name="img"
         value={formData.img}
